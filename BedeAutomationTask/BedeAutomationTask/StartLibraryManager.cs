7d0c78063111c583d56ec930cf8b8ba9838d4f6e
Kaloyan Bratoev <kaloyan.bratoev@gmail.com>
2018-07-11 07:39:31 +0300

    BedeGaming structure

---
+++ BedeAutomationTask/BedeAutomationTask/StartLibraryManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using System.Diagnostics;
+
+namespace BedeAutomationTask
+{
+    [Binding]
+    public class StartLibraryManager
+    {
+        [Given(@"the Library Manager is started")]
+        public void GivenTheLibraryManagerIsStarted()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
